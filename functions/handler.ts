82c13083bec93bb03cbbb66f5dc5989738fb686e
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-01-22 19:08:44 +0900

    add

--- functions/handler.ts
+++ functions/handler.ts
@@ -3,6 +3,9 @@ const hello = require("../lib")
 export const handler = async (): Promise<any> => {
   const r = hello()
   console.log('Hello World!', r);
-  return { r };
+  return {
+    statusCode: 200,
+    body: "Hello, World" + r
+  }
 }
 
